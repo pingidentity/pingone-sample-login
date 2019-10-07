f723b1106821b4cbbd669a4a7aaed651e34ca47a
mdeller-ping <34523087+mdeller-ping@users.noreply.github.com>
2019-10-07 13:18:21 -0400

    Add files via upload

--- js/auth.js
+++ js/auth.js
@@ -1,8 +1,8 @@
 // variable definitions and building authorization url
 
-const environmentId = 'b9da0edc-794c-453f-8988-f09426f36865'; // available on settings page of p14c admin console
-const clientId = '5f5ca4fb-ac2e-4f2f-bcf6-fcea98a8b8c0'; // available on connections tab of admin console
-const baseUrl = 'https://pingidentity.github.io/pingone-customers-sample-login/'; // URL of where you will host this application
+const environmentId = ''; // available on settings page of p14c admin console
+const clientId = ''; // available on connections tab of admin console
+const baseUrl = 'http://localhost/'; // URL of where you will host this application
 
 const scopes = 'openid profile'; // default scopes to request
 const responseType = 'token id_token';
@@ -24,7 +24,6 @@ if (!clientId || !environmentId) {
 // doLogin function: generates and stores nonce, redirects to authorization request url
 
 function doLogin() {
-  console.log('doLogin called');
   let nonce = generateNonce(60);
   let authorizationUrl =
     authUrl +
@@ -41,11 +40,7 @@ function doLogin() {
     '&nonce=' +
     nonce;
 
-  console.log(authorizationUrl);
-  
-  window.localStorage.setItem('nonce', nonce);
-
-  // window.localStorage.setItem('nonce', 'bogus nonce that does not match');
+  Cookies.set('nonce', nonce, { domain: cookieDomain });
 
   window.location.href = authorizationUrl;
 }
